5a81bb93d04ee229bef15a9921fec293237e6dff
Franck <contact@franckwebpro.com>
2024-06-11 15:07:02 +0200

    css homesection

--- client/src/components/HomeSection.jsx
+++ client/src/components/HomeSection.jsx
@@ -4,15 +4,16 @@ export default function HomeSection() {
   return (
     <section className="w-full flex flex-col items-center justify-center md:items-start gap-2">
       <img
-        src="./src/assets/images/coureur.webp"
+        src="./src/assets/images/runner.webp"
         alt="a runner"
-        className="img-shadow w-full md:w-3/5"
+        className="img-shadow w-[calc(100%-15px)] mr-auto md:w-4/6 lg:w-1/2
+         rounded-xl mb-4"
       />
       <h2>Votre sport en Streaming</h2>
       <p>Les avantages...</p>
       <Link
         to="/register"
-        className="text-[var(--lightColor)] text-xl lg:text-2xl visited:text-[var(--lightColor)] bg-[var(--primaryLight)] p-2 md:p-3 lg:p-4 rounded-xl 
+        className="text-[var(--lightColor)] text-xl lg:text-2xl visited:text-[var(--lightColor)] bg-[var(--primaryColor)] p-2 md:p-3 lg:p-4 rounded-xl 
         flex item-center justify-center gap-2 mt-2"
       >
         <img
