7ef617ab27b757aee90708016cab2927382e8474
Rollet Fabien <fabrol47@gmail.com>
2024-06-13 21:40:14 +0200

    added link, changed global link css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,52 +1,93 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <footer className="bg-[var(--primaryColor)] p-4 lg:flex lg:p-10 " >
-      <h2 className="italic font-extrabold uppercase">Sweet Stream</h2>
+    <footer className="bg-[var(--primaryColor)] p-4 lg:flex lg:p-10 ">
+      <h2 className="italic font-extrabold uppercase">
+        <Link to="/">Sweet Stream</Link>
+      </h2>
       <ul className="flex flex-wrap gap-x-6 gap-y-6 lg:grow lg:justify-evenly lg:leading-10">
         <li className="grow lg:grow-0">
-          <h3>Pages</h3>
+          <h3>
+            <Link to="/">Pages</Link>
+          </h3>
           <ul>
-            <li>Accueil</li>
-            <li>Pourquoi s'abonner</li>
-            <li>Inscription utilisateur</li>
-            <li>Inscription coach</li>
-            <li>Connexion</li>
+            <li>
+              <Link to="/">Accueil</Link>
+            </li>
+            <li>
+              <Link to="/">Pourquoi s'abonner</Link>
+            </li>
+            <li>
+              <Link to="/">Inscription utilisateur</Link>
+            </li>
+            <li>
+              <Link to="/">Inscription coach</Link>
+            </li>
+            <li>
+              <Link to="/">Connexion</Link>
+            </li>
           </ul>
         </li>
         <li className="grow lg:grow-0">
           <ul>
-            <h3>Catégories</h3>
-            <li>Musculation</li>
-            <li>Pilate</li>
-            <li>Yoga</li>
-            <li>Fitness</li>
-            <li>Nutrition</li>
+            <h3>
+              <Link to="/">Catégories</Link>
+            </h3>
+            <li>
+              <Link to="/">Musculation</Link>
+            </li>
+            <li>
+              <Link to="/">Pilate</Link>
+            </li>
+            <li>
+              <Link to="/">Yoga</Link>
+            </li>
+            <li>
+              <Link to="/">Fitness</Link>
+            </li>
+            <li>
+              <Link to="/">Nutrition</Link>
+            </li>
           </ul>
         </li>
         <li className="grow lg:grow-0">
-          <h3>Réseaux</h3>
+          <h3>
+            <Link to="/">Réseaux</Link>
+          </h3>
           <ul className="flex justify-between items-start lg:block m-0">
-            <li>Contact</li>
+            <li>
+              <Link to="/">Contact</Link>
+            </li>
             <li className="m-0">
               <section className="pt-0 lg:px-0">
                 <ul className="flex justify-around lg:justify-start">
                   <li className="m-0">
-                    <img
-                      src="src/assets/images/icons8-facebook-100.png"
-                      alt="" className="w-10"
-                    />
+                    <Link to="/">
+                      <img
+                        src="src/assets/images/icons8-facebook-100.png"
+                        alt=""
+                        className="w-10"
+                      />
+                    </Link>
                   </li>
                   <li>
-                    <img
-                      src="src/assets/images/icons8-instagram-100.png"
-                      alt="" className="w-10"
-                    />
+                    <Link to="/">
+                      <img
+                        src="src/assets/images/icons8-instagram-100.png"
+                        alt=""
+                        className="w-10"
+                      />
+                    </Link>
                   </li>
                   <li>
-                    <img
-                      src="src/assets/images/icons8-tic-tac-100.png"
-                      alt="" className="w-10"
-                    />
+                    <Link to="/">
+                      <img
+                        src="src/assets/images/icons8-tic-tac-100.png"
+                        alt=""
+                        className="w-10"
+                      />
+                    </Link>
                   </li>
                 </ul>
               </section>
