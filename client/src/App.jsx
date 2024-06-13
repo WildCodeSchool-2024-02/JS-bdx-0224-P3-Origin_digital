ed3c62b0cb33b4dbc8a1d21221b599c4be5954b0
sachaig <sacha.isambourg01@gmail.com>
2024-06-13 14:13:46 +0200

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <Outlet />;
+  return (
+  <>
+  <Header />
+  <main>
+    <Outlet />
+  </main>
+  <Footer />
+  </>
+  );
 }
 
 export default App;
