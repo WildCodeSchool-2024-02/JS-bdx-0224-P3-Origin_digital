dd34ed7fc0b1e244d474a93eed5da7b8baa3ef1f
Rollet Fabien <fabrol47@gmail.com>
2024-06-14 09:37:10 +0200

    typo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-[var(--primaryColor)] p-4 lg:flex lg:p-10 ">
       <h2 className="italic font-extrabold uppercase">
-        <Link to="/">Sweet Stream</Link>
+        <Link to="/">Sweat Stream</Link>
       </h2>
       <ul className="flex flex-wrap gap-x-6 gap-y-6 lg:grow lg:justify-evenly lg:leading-10">
         <li className="grow lg:grow-0">
