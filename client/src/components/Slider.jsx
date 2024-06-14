e798b16d3f39786e1af004e3ef65cc7d24ce2b09
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-06-14 11:30:51 +0200

    fixed css on swiper slider

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable import/no-unresolved */
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
-import { Link } from "react-router-dom";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import image1 from "../assets/images/cardio.webp";
-import image2 from "../assets/images/bien_etre.webp";
-import image3 from "../assets/images/mae_coach_fitness.webp";
-import image4 from "../assets/images/minceur.webp";
-import image5 from "../assets/images/prenatal.webp";
+import "../assets/styles/slider.css";
+import { Navigation, Pagination } from "swiper/modules";
+import { Link } from "react-router-dom";
+import image1 from "../assets/images/musculation.jpg";
+import image2 from "../assets/images/yoga.jpg";
+import image3 from "../assets/images/nutrition.jpg";
+import image4 from "../assets/images/pilat.jpg";
+import image5 from "../assets/images/training.jpg";
 
 const sportList = [
   {
@@ -26,23 +27,37 @@ const sportList = [
   },
   {
     imgSrc: image4,
-    name: "Pilat",
+    name: "Pilates",
   },
   {
     imgSrc: image5,
-    name: "Pilat",
+    name: "Fitness",
   },
 ];
 
 export default function Slider() {
   return (
     <Swiper
-      modules={[Navigation, Pagination]}
       spaceBetween={30}
-      slidesPerView={4}
       loop
       navigation
       pagination={{ clickable: true }}
+      modules={[Navigation, Pagination]}
+      breakpoints={{
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+      }}
+      className="mySwiper"
     >
       {sportList.map((sport) => (
         <SwiperSlide key={sport.name}>
@@ -50,12 +65,12 @@ export default function Slider() {
             <img
               src={sport.imgSrc}
               alt={sport.name}
-              className="img-shadow w-[calc(100%-15px)] mr-auto rounded-xl mb-4"
+              className="img-shadow w-[calc(100%-15px)] mr-auto rounded-xl mb-4 h-60 object-cover"
             />
-            <p className="flex justify-center items-center font-bold">
-              {sport.name}
-            </p>
           </Link>
+          <p className="flex justify-center items-center font-bold">
+            {sport.name}
+          </p>
         </SwiperSlide>
       ))}
     </Swiper>
