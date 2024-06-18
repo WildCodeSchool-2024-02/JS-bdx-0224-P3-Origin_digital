e229bac83dd1811711ff7a459fb9510bc1a87369
sachaig <sacha.isambourg01@gmail.com>
2024-06-18 11:09:09 +0200

    css fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,26 +32,25 @@ function Header() {
     ${isMenuOpen ? "opacity-100 translate-y-0" : "-translate-y-[100vh]"}`;
 
   const signUpClasses = `mr-5 flex items-center transform transition-all duration-300 fixed 
-  lg:static lg:max-h-16 lg:flex-row lg:justify-around lg:opacity-100 lg:transform-none
-  ${isMenuOpen ? "opacity-100 transform translate-y-0" : "-translate-y-[100vh]"}`;
+    lg:static lg:max-h-16 lg:flex-row lg:justify-around lg:opacity-100 lg:transform-none
+    ${isMenuOpen ? "opacity-100 transform translate-y-0" : "-translate-y-[100vh]"}`;
 
   const burgerButtonClasses = `burgerMenu relative w-10 h-7 bg-transparent text-0 hover:bg-transparent none border-none 
-  flex justify-self-end 
-  ${isMenuOpen ? "active" : ""}`;
+    flex justify-self-end 
+    ${isMenuOpen ? "active" : ""}`;
 
-  const objectiveSectionClasses = `absolute top-[4.5rem] left-0 right-0 bottom-0 w-full 
-  uppercase bg-white list-none justify-around items-center transform transition-all duration-300 text-center 
-  lg:top-[6rem] lg:flex-row 
+    const objectiveSectionClasses = `absolute top-[4.5rem] left-0 right-0 bottom-0 w-full 
+    uppercase bg-white list-none justify-around items-center transform transition-all duration-300 text-center 
+    lg:top-[6rem] lg:flex-row 
   ${isObjectivesMenuOpen ? "opacity-100 transform translate-y-0" : "opacity-0 pointer-events-none"}`;
 
   const menuListClasses = `top-[4.5rem] h-[calc(100vh-4.5rem)] w-full uppercase bg-white list-none 
-  flex flex-col justify-around items-center 
-  transform -translate-y-[100vh] transition-all duration-300 text-center fixed inset-0
-  lg:bg-[var(--secondaryColor)] lg:static lg:max-h-12 lg:flex-row lg:justify-around lg:opacity-100 lg:transform-none 
-  ${isMenuOpen ? "opacity-100 transform translate-y-0" : ""}`;
+    flex flex-col justify-around items-center 
+    transform -translate-y-[100vh] transition-all duration-300 text-center fixed inset-0
+    lg:bg-[var(--secondaryColor)] lg:static lg:max-h-12 lg:flex-row lg:justify-around lg:opacity-100 lg:transform-none 
+    ${isMenuOpen ? "opacity-100 transform translate-y-0" : ""}`;
 
-  const objectivesButtonClasses =
-    `font-[var(--secondaryFont)] text-[var(--darkColor)] no-underline bg-transparent 
+  const objectivesButtonClasses = `font-[var(--secondaryFont)] text-[var(--darkColor)] no-underline bg-transparent 
     hover:bg-transparent hover:text-[var(--darkColor)] 
     p-0 border-none cursor-pointer`;
 
