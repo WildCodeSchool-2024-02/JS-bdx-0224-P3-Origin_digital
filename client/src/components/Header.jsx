9da6051f0ba58cb857030c20570a2a591512aed8
sachaig <sacha.isambourg01@gmail.com>
2024-06-18 10:05:07 +0200

    clean fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -38,7 +38,8 @@ function Header() {
   const burgerButtonClasses = `burgerMenu relative w-10 h-7 bg-transparent text-0 hover:bg-transparent none flex border-none justify-self-end 
   ${isMenuOpen ? "active" : ""}`;
 
-  const objectiveSectionClasses = `absolute top-[4.5rem] left-0 right-0 bottom-0 w-full uppercase bg-white list-none justify-around items-center transform transition-all duration-300 text-center 
+  const objectiveSectionClasses = `absolute top-[4.5rem] left-0 right-0 bottom-0 w-full 
+  uppercase bg-white list-none justify-around items-center transform transition-all duration-300 text-center 
   lg:top-[6rem] lg:flex-row 
   ${isObjectivesMenuOpen ? "opacity-100 transform translate-y-0" : "opacity-0 pointer-events-none"}`;
 
@@ -49,7 +50,9 @@ function Header() {
   ${isMenuOpen ? "opacity-100 transform translate-y-0" : ""}`;
 
   const objectivesButtonClasses =
-    "font-[var(--secondaryFont)] text-[var(--darkColor)] no-underline bg-transparent hover:bg-transparent hover:text-[var(--darkColor)] p-0 border-none cursor-pointer";
+    `font-[var(--secondaryFont)] text-[var(--darkColor)] no-underline bg-transparent 
+    hover:bg-transparent hover:text-[var(--darkColor)] 
+    p-0 border-none cursor-pointer`;
 
   return (
     <Navbar
