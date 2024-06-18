be67084f83346d9f6455c40949ac2fbcfdac5951
sachaig <sacha.isambourg01@gmail.com>
2024-06-18 09:16:43 +0200

    Header complete

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,53 +1,155 @@
-import { useState } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import logoSrc from "../assets/images/LogoSweatStream.png";
 
-function Navbar() {
-  const [menuState, setMenuState] = useState(false);
-
-  const closeMenu = () => {
-    setMenuState(false);
-  };
-
+function Navbar({
+  isObjectivesMenuOpen,
+  closeMenu,
+  handleClick,
+  handleChange,
+  signInClasses,
+  signUpClasses,
+  burgerButtonClasses,
+  menuListClasses,
+  objectivesButtonClasses,
+  objectiveSectionClasses,
+}) {
   return (
-    <header className="bg-[var(--secondaryColor)] flex items-center justify-between border-b">
-      <nav id="navbar">
+    <header className="bg-[var(--secondaryColor)] h-20 lg:h-24 flex items-center">
+      <nav
+        id="navbar"
+        className="grid grid-cols-2 items-center w-full h-[4.5rem] px-4 lg:px-8  lg:flex"
+      >
         <Link to="/">
           <img
-            className="w-full lg:w-1/2"
+            className="~w-[250px]/[350px] "
             src={logoSrc}
             alt="retour à l'accueil"
           />
         </Link>
-        <button type="button" onClick={() => setMenuState(!menuState)}>
-          {menuState ? "x" : "≡"}
+        <button
+          type="button"
+          onClick={handleClick}
+          className={burgerButtonClasses}
+          title="Menu déroulant"
+        >
+          Menu déroulant
+          <span className="line-burger block h-0.5 w-8 bg-gray-600 " />
+          <span className="line-burger block h-0.5 w-8 bg-gray-600" />
+          <span className="line-burger block h-0.5 w-8 bg-gray-600 " />
         </button>
 
-        <ul className={`burger-menu${menuState ? " active" : ""}`}>
+        <ul className={menuListClasses}>
           <li>
-            <Link to="/" onClick={closeMenu}>
-              Objectif
-            </Link>
+            <button
+              onClick={handleChange}
+              className={objectivesButtonClasses}
+              type="button"
+            >
+              Objectifs
+            </button>
           </li>
           <li>
-            <Link to="/" onClick={closeMenu}>
+            <Link
+              to="/"
+              onClick={closeMenu}
+              className="font-[var(--secondaryFont)] text-[var(--darkColor)] no-underline"
+            >
               Pourquoi s'abonner ?
             </Link>
           </li>
           <li>
-            <Link to="/" onClick={closeMenu}>
+            <Link
+              to="/"
+              onClick={closeMenu}
+              className="font-[var(--secondaryFont)] text-[var(--darkColor)] no-underline"
+            >
               Mon espace
-            </Link>
-          </li>
-          <li>
-            <Link to="/" onClick={closeMenu}>
-              Cocktails
             </Link>
           </li>
         </ul>
       </nav>
+      <button className={signInClasses} type="button">
+        {" "}
+        <Link to="/">inscription</Link>
+      </button>
+      <button className={signUpClasses} type="button">
+        <Link to="/">connexion</Link>
+      </button>
+      {isObjectivesMenuOpen && (
+        <div className={objectiveSectionClasses}>
+          <button
+            className="absolute top-0 right-0 px-6 py-6 bg-transparent hover:bg-transparent"
+            type="button"
+            aria-label="bouton fermant dans objectif"
+            onClick={handleChange}
+          >
+            <svg
+              className="h-8 w-8 text-gray-600"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </button>
+          <ul className="flex flex-col items-center justify-between bg-transparent">
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <Link
+                to="/category1"
+                className="text-[var(--darkColor)] visited:text-[var(--darkColor)]"
+              >
+                yoga
+                <ul>
+                  <li>#tag1</li>
+                  <li>#tag1</li>
+                  <li>#tag1</li>
+                </ul>
+              </Link>
+              <ul>
+                <li>#tag1</li>
+                <li>#tag1</li>
+                <li>#tag1</li>
+              </ul>
+            </li>
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <Link
+                to="/category2"
+                className="text-[var(--darkColor)] visited:text-[var(--darkColor)]"
+              >
+                Musculation
+              </Link>
+            </li>
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <Link
+                to="/category3"
+                className="text-[var(--darkColor)] visited:text-[var(--darkColor)]"
+              >
+                Pilates
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
+
+Navbar.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  isObjectivesMenuOpen: PropTypes.bool.isRequired,
+  closeMenu: PropTypes.func.isRequired,
+  signInClasses: PropTypes.string.isRequired,
+  signUpClasses: PropTypes.string.isRequired,
+  burgerButtonClasses: PropTypes.string.isRequired,
+  menuListClasses: PropTypes.string.isRequired,
+  objectivesButtonClasses: PropTypes.string.isRequired,
+  objectiveSectionClasses: PropTypes.string.isRequired,
+};
 
 export default Navbar;
