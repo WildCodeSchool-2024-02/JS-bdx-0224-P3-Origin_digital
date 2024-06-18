4a7f625d6a73bb0bb58fb6720f39dde60e5c44c9
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-06-18 17:46:52 +0200

    create subscription page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import HeroSection from "../components/HeroSection";
 import RegisterSection from "../components/registerSection";
 import InfoSection from "../components/infoSection";
 import Slider from "../components/Slider";
-import SubscriptionPage from "./SubscriptionPage";
 
 function Home() {
   return (
