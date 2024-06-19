bb65727062cf093bf7a3ddc2c1844bfd6adbcfad
Rollet Fabien <fabrol47@gmail.com>
2024-06-19 16:02:26 +0200

    wip dashboard

--- client/src/components/DashboardVideo.jsx
+++ client/src/components/DashboardVideo.jsx
@@ -1,18 +1,16 @@
-import { Checkbox, Cell, Row } from "react-aria-components";
+import { Cell, Row } from "react-aria-components";
+import { Link } from 'react-router-dom';
 
 export default function DashboardVideo() {
   return (
     <Row>
-      <Cell>
-        <Checkbox slot="selection" />
-      </Cell>
       <Cell className="flex items-center">
         <img src="../src/assets/images/pilat.jpg" alt="" className="w-36" />
         <p className="px-4">Titre de la vidéo</p>
       </Cell>
       <Cell>Pilate</Cell>
       <Cell>
-        <ul>
+        <ul className="p-0 flex flex-wrap w-auto h-24 justify-center items-center overflow-auto gap-2  [&>*]:py-0 [&>*]:px-4 [&>*]:text-xl [&>*]:h-7 [&>*]:bg-[var(--primaryColor)] [&>*]:rounded-3xl [&>*]:text-[var(--darkColor)] [&>*]:font-bold [&>*]:capitalize">
           <li>#Tag1</li>
           <li>#Tag2</li>
           <li>#Tag3</li>
@@ -21,6 +19,12 @@ export default function DashboardVideo() {
       <Cell>Abonné</Cell>
       <Cell>301</Cell>
       <Cell>18/06/2024</Cell>
+      <Cell>
+        <ul className="flex flex-col gap-4">
+          <li><Link to="/">Modifier</Link></li>
+          <li><Link to="/">Supprimer</Link></li>
+        </ul>
+      </Cell>
     </Row>
   );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,4 @@
-import {
-  Checkbox,
-  Column,
-  Table,
-  TableHeader,
-  TableBody,
-} from "react-aria-components";
+import { Column, Table, TableHeader, TableBody } from "react-aria-components";
 import DashboardVideo from "../components/DashboardVideo";
 
 export default function Dashboard() {
@@ -16,19 +10,19 @@ export default function Dashboard() {
         className="bg-(var"
       />
       <button type="button"> + </button>
-      <Table aria-label="Files" selectionMode="multiple">
+      <section className="overflow-x-auto rounded-xl">
+
+      <Table aria-label="Files" selectionMode="multiple" >
         <TableHeader className=" bg-[var(--primaryLight)]">
-          <Column className="px-20">
-            <Checkbox slot="selection" />
-          </Column>
-          <Column isRowHeader className="px-20">
+          <Column isRowHeader className="w-60">
             Vos vidéos
           </Column>
-          <Column className="px-20">Catégorie</Column>
-          <Column className="px-20">Tag</Column>
-          <Column className="px-20">Visibilité</Column>
-          <Column className="px-10">Vue</Column>
-          <Column className="px-10">Publication</Column>
+          <Column className="w-28">Catégorie</Column>
+          <Column className="w-56">Tag</Column>
+          <Column className="w-28">Visibilité</Column>
+          <Column className="w-16">Vue</Column>
+          <Column className="w-36">Publication</Column>
+          <Column className="w-36">Modification</Column>
         </TableHeader>
         <TableBody>
           <DashboardVideo />
@@ -37,6 +31,7 @@ export default function Dashboard() {
           <DashboardVideo />
         </TableBody>
       </Table>
+      </section>
     </section>
   );
 }
