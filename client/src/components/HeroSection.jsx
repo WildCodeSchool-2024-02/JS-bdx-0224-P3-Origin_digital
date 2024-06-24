27e83478bd4634b8a594f88336c6ee429cfe3e21
Rollet Fabien <fabrol47@gmail.com>
2024-06-24 22:55:07 +0200

    removed comment

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -3,11 +3,6 @@ import { Link } from "react-router-dom";
 export default function HeroSection() {
   return (
     <>
-    {/* <header
-      className="h-screen w-full flex flex-col items-center justify-center bg-[url(./assets/images/heroBg.webp)]  
-      bg-no-repeat bg-[30%] bg-cover bg-clip-border gap-2 lg:grid lg:grid-cols-[repeat(2,minmax(0,1fr)),1.5fr] 
-      lg:grid-rows-[repeat(3,minmax(0,auto))] py-48 lg:pr-8"
-    > */}
       <h1 className="w-full text-[var(--lightColor)] text-5xl md:text-6xl bg-[url(./assets/images/heroBg.webp)] bg-no-repeat bg-[30%] bg-cover bg-clip-border h-dvh text-center lg:text-left lg:pl-[55%] pt-[20%] lg:pt-[10%] drop-shadow-2xl">
         Votre sport en Streaming
       </h1>
