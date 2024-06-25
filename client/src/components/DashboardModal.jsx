4a8545e979b8acb09893f67176678ff18bc834c0
Franck <contact@franckwebpro.com>
2024-06-25 16:48:22 +0200

    finished modal CSS

--- client/src/components/DashboardModal.jsx
+++ client/src/components/DashboardModal.jsx
@@ -26,6 +26,7 @@ export default function DashboardModal({
 }) {
   const [selectedAccess, setSelectedAccess] = useState("Public");
   const [selectedTags, setSelectedTags] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const handleClickAccessSelection = () => {
     setSelectedAccess(selectedAccess === "Public" ? "Abonnés" : "Public");
@@ -39,17 +40,31 @@ export default function DashboardModal({
     <FocusLock>
       <dialog
         className={` ${displayClass} absolute top-0 left-0 w-screen min-h-screen bg-[var(--primaryColor)] 
-       grid-cols-[1fr,0.25fr] grid-rows-[70px,auto] items-center justify-center p-4 md:px-32 lg:py-16 lg:px-[25vw] lg:bg-[var(--blurBg)]`}
+       grid-cols-[1fr,0.25fr] grid-rows-[70px,auto] items-center justify-center p-4 md:px-32 lg:py-12 lg:px-[25vw] 
+       lg:bg-[var(--blurBg)]`}
         open={isModalOpen}
-        aria-modal="true"
+        onClick={handleOpenModal}
+        onKeyDown={(e) => {
+          if (e.key === "Escape") {
+            handleOpenModal();
+          }
+        }}
+        role="presentation"
       >
-        <h2 id="form-title" className="w-full col-span-1 lg:pl-2">
+        <h2
+          id="form-title"
+          className="w-full col-span-1 lg:pl-2"
+          onClick={(e) => e.stopPropagation()}
+          onKeyDown={(e) => {
+            e.stopPropagation();
+          }}
+          role="presentation"
+        >
           {toModify ? "Modifier la vidéo" : "Ajouter une vidéo"}
         </h2>
         <button
-          className="bg-none col-span-1 place-self-start justify-self-end text-[var(--lightColor)] 
-        bg-[var(--primaryDark)] hover:text-[var(--darkColor)] hover:bg-[var(--secondaryColor)] 
-        focus:border-4 focus:border-green-500"
+          className="col-span-1 place-self-start justify-self-end text-[var(--lightColor)] focus:outline focus:outline-2 focus:outline-blue-600
+        bg-[var(--primaryDark)] hover:text-[var(--darkColor)] hover:bg-[var(--primaryLight)]"
           onClick={handleOpenModal}
           type="button"
           aria-label="fermer la modale"
@@ -58,72 +73,67 @@ export default function DashboardModal({
         </button>
         <Form
           aria-labelledby="form-title"
-          className="w-full h-full flex flex-col items-center justify-evenly col-span-2 gap-1 
-        md:gap-2 lg:gap-6 lg:p-4 lg:rounded-3xl lg:bg-[var(--primaryColor)]"
+          className="w-full h-full flex flex-col items-center justify-evenly col-span-2 gap-1 md:gap-x-4 md:grid md:grid-cols-[1.5fr,1fr] 
+          md:grid-rows-[repeat(6, minmax(0, auto))] md:gap-2 lg:gap-x-5 lg:p-4 lg:rounded-3xl lg:bg-[var(--primaryColor)]"
+          onClick={(e) => e.stopPropagation()}
         >
-          <FileTrigger name="video_url" acceptedFileTypes={["video/mp4"]}>
-            <Button
-              className="bg-[var(--primaryColor)] w-full border-dashed border border-black text-base 
-          normal-case min-h-11 md:min-h-12"
-            >
-              Enregistrer votre vidéo
-            </Button>
-          </FileTrigger>
-          <FileTrigger
-            name="preview_url"
-            acceptedFileTypes={["image/png", "image/webp"]}
-          >
-            <Button
-              className="bg-[var(--primaryColor)] w-full border-dashed border border-black 
-          text-base min-h-11 md:min-h-12"
-            >
-              Enregistrer votre miniature
-            </Button>
-          </FileTrigger>
           <TextField
             name="title"
             type="text"
             isRequired
             necessityIndicator="icon"
-            className="w-full"
+            className="w-full col-[1/2]"
           >
-            <Label className="block mb-1 text-base">Titre*</Label>
+            <Label className="block mb-1 text-base font-nunitoBold">
+              Titre*
+            </Label>
             <Input
-              className="bg-[var(--lightColor)] w-full min-h-11 rounded-xl p-1 outline-none focus:border 
-          focus:border-[var(--primaryDark)] md:min-12"
+              className="bg-[var(--lightColor)] text-sm w-full min-h-10 rounded-xl p-1 outline-none ease-linear duration-100 
+              hover:border hover:border-[var(--primaryDark)] focus:outline focus:outline-2 focus:outline-blue-600 md:min-h-12 md:text-base"
             />
-            <FieldError />
+            <FieldError className="text-sm pl-2" />
           </TextField>
-          <TextField className="w-full" isRequired necessityIndicator="icon">
-            <Label className="block mb-1 text-base">Description*</Label>
+          <TextField
+            className="w-full md:col-[1/2]"
+            isRequired
+            necessityIndicator="icon"
+          >
+            <Label className="block mb-1 text-base font-nunitoBold">
+              Description*
+            </Label>
             <TextArea
               label="Description"
               name="description"
-              className="bg-[var(--lightColor)] w-full radius-4 rounded-[15px] min-h-12 p-1
-            outline-none focus:border focus:border-[var(--primaryDark)] md:min-h-16"
+              className="bg-[var(--lightColor)] w-full radius-4 text-sm rounded-[15px] min-h-10 p-1
+            outline-none ease-linear duration-100 focus:outline focus:outline-2 focus:outline-blue-600 hover:border 
+            hover:border-[var(--primaryDark)] md:min-h-16 md:text-base "
             />
+            <FieldError className="text-sm pl-2" />
           </TextField>
-          <Label className="w-full text-base">
+          <Label className="w-full text-base font-nunitoBold md:col-[1/2]">
             Catégories*
             <Select
-              className="w-full relative mt-1 min-h-11 md:min-h-12"
+              className="w-full relative mt-1 min-h-10 font-nunito md:min-h-12"
               aria-label="selection de tag"
             >
-              <Button className="w-full bg-[var(--lightColor)] flex justify-between items-center min-h-11 md:min-h-12">
+              <Button
+                className="w-full bg-[var(--lightColor)] flex justify-between items-center min-h-10 
+                focus:outline focus:outline-2 focus:outline-blue-600 md:min-h-12"
+              >
                 <SelectValue placeholder="Choisissez une catégorie" />
-                <span aria-hidden="true" className="justify-self-end">
+                <span aria-hidden="true" className="justify-self-end ">
                   ▼
                 </span>
               </Button>
               <Popover
                 className="w-4/5 bg-[var(--lightColor)] border border-[var(--darkColor)] rounded-[15px] 
-              py-2 px-4 md:w-1/2 lg:w-1/3"
+              py-2 px-4 md:w-2/3 lg:w-1/3 "
               >
                 <ListBox
                   aria-label="selection de tag"
                   selectionMode="multiple"
-                  selectedKeys={selectedTags}
-                  onSelectionChange={setSelectedTags}
+                  selectedKeys={selectedCategory}
+                  onSelectionChange={setSelectedCategory}
                 >
                   <ListBoxItem className="hover:bg-slate-200 cursor-pointer w-full p-1 rounded-md">
                     Aardvark
@@ -137,16 +147,25 @@ export default function DashboardModal({
               </Popover>
             </Select>
           </Label>
-          <Label className="w-full text-base">
+          <Label className="w-full text-base font-nunitoBold md:col-[1/2]">
             Tags*
-            <Select className="w-full mt-1" aria-label="selection de tag">
-              <Button className="w-full bg-[var(--lightColor)] flex justify-between items-center min-h-11 md:min-h-12">
+            <Select
+              className="w-full mt-1 font-nunito"
+              aria-label="selection de tag"
+            >
+              <Button
+                className="w-full bg-[var(--lightColor)] flex justify-between items-center min-h-10 md:min-h-12 
+              focus:outline focus:outline-2 focus:outline-blue-600"
+              >
                 <SelectValue placeholder="Choisissez une catégorie" />
                 <span aria-hidden="true" className="justify-self-end">
                   ▼
                 </span>
               </Button>
-              <Popover className="w-4/5 bg-[var(--lightColor)]  border border-[var(--darkColor)] rounded-[15px] py-2 px-4">
+              <Popover
+                className="w-4/5 bg-[var(--lightColor)]  border border-[var(--darkColor)] rounded-[15px] 
+              py-2 px-4 md:w-2/3 lg:w-1/3"
+              >
                 <ListBox
                   aria-label="selection de tag"
                   selectionMode="multiple"
@@ -163,36 +182,64 @@ export default function DashboardModal({
               </Popover>
             </Select>
           </Label>
-          <Label className="w-full text-base flex flex-wrap gap-2 items-center">
-            <span className="w-full">Accès</span>
+          <Label className="w-full flex flex-wrap gap-2 items-center outline-none focus:outline focus:outline-2 focus:outline-blue-600 md:col-[1/2]">
+            <span className="w-full font-nunitoBold">Accès</span>
             <span
-              className={
+              className={`text-sm md:text-base ${
                 selectedAccess === "Public"
-                  ? "text-blue-600 font-bold"
+                  ? "text-blue-600 font-nunitoBold"
                   : "font-light"
-              }
+              }`}
             >
               Public
             </span>
             <input
               value={selectedAccess}
               type="checkbox"
-              className="theme-checkbox"
+              className="theme-checkbox outline-none focus:outline focus:outline-2 focus:outline-blue-600"
               onChange={handleClickAccessSelection}
             />{" "}
             <span
-              className={
+              className={`text-sm md:text-base ${
                 selectedAccess === "Abonnés"
-                  ? "text-blue-600 font-bold"
+                  ? "text-blue-600 font-nunitoBold"
                   : "font-light"
-              }
+              }`}
             >
               Abonnés
             </span>
           </Label>
+          <fieldset
+            className="w-full flex flex-wrap items-center justify-center gap-3 md:justify-evenly md:gap-5 
+          md:flex-col md:col-[2/-1] md:row-[1/6] md:h-[90%]"
+          >
+            <legend className="w-full mb-1 text-base font-nunitoBold md:mb-2">
+              Télécharger vos fichiers
+            </legend>
+            <FileTrigger name="video_url" acceptedFileTypes={["video/mp4"]}>
+              <Button
+                className="insertField w-full mb-2 border-black text-base 
+          normal-case min-h-10 focus:outline focus:outline-2 focus:outline-blue-600 md:min-h-14 md:h-[40%] md:mb-0"
+              >
+                Inserer votre vidéo
+              </Button>
+            </FileTrigger>
+            <FileTrigger
+              name="preview_url"
+              acceptedFileTypes={["image/png", "image/webp"]}
+            >
+              <Button
+                className="insertField w-full border-black 
+          text-base min-h-10 focus:outline focus:outline-2 focus:outline-blue-600 md:min-h-14 md:h-[40%]"
+              >
+                Inserer votre miniature
+              </Button>
+            </FileTrigger>
+          </fieldset>
           <Button
             type="submit"
-            className="w-full min-h-11 md:min-h-14 text-[var(--lightColor)] bg-[var(--primaryDark)] hover:text-[var(--darkColor)] hover:bg-[var(--secondaryColor)]"
+            className="w-full min-h-10 md:min-h-14 text-[var(--lightColor)] bg-[var(--primaryDark)] outline-none
+            hover:text-[var(--darkColor)] hover:bg-[var(--secondaryColor)] focus:outline focus:outline-2 focus:outline-blue-600 md:col-[1/-1]"
           >
             {toModify ? "Modifier" : "Ajouter"}
           </Button>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,13 +9,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        'pc':'#e1b7ff',
-        'pcd':'#b44dfe',
-        'pcl':'#f2e2fe',
-        'sc':'#eaf3f5',
-        'lc':'#fdfdfd',
-        'dc':'#353535',
-      }
+        pc: "#e1b7ff",
+        pcd: "#b44dfe",
+        pcl: "#f2e2fe",
+        sc: "#eaf3f5",
+        lc: "#fdfdfd",
+        dc: "#353535",
+      },
+      fontFamily: {
+        nunito: ["Nunito-Sans", "sans-serif"],
+        rajdhani: ["Rajdhani", "sans-serif"],
+        nunitoBold: ["Nunito-Sans-Bold", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderColor: ["focus"],
+      outline: ["focus"],
     },
   },
   plugins: [fluid],
