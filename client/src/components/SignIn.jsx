3314977007296f084c9691ccf418416de1b66ac9
Rollet Fabien <fabrol47@gmail.com>
2024-06-26 14:51:11 +0200

    better naming but in english this time

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import fitnessImg from "../assets/images/training.jpg";
 import "../assets/styles/form.css";
 
-function SignIn({ handleChangeSaisie, fields, formValues, generateFieldLabelClass }) {
+function SignIn({ handleChangeScribe, fields, formValues, generateFieldLabelClass }) {
   return (
     <section className="flex justify-center items-center gap-10 ">
       <figure className="hidden lg:flex justify-center">
@@ -23,7 +23,7 @@ function SignIn({ handleChangeSaisie, fields, formValues, generateFieldLabelClas
                 id={info.id}
                 name={info.text}
                 value={formValues[info.id]}
-                onChange={handleChangeSaisie}
+                onChange={handleChangeScribe}
                 className="peer border-b-2 border-dark-color py-1 focus:border-b-2 focus:border-primary-color transition-colors focus:outline-none bg-inherit w-full"
               />
               <label
@@ -45,7 +45,7 @@ function SignIn({ handleChangeSaisie, fields, formValues, generateFieldLabelClas
 }
 
 SignIn.propTypes = {
-  handleChangeSaisie: PropTypes.func.isRequired,
+  handleChangeScribe: PropTypes.func.isRequired,
   fields: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
