43a4c2dc818e3f2e49c662511e03daeeb6f06b34
sachaig <sacha.isambourg01@gmail.com>
2024-06-26 16:25:41 +0200

    navbar visible again

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,14 +35,14 @@ function Header() {
 
   const objectiveSectionClasses = `absolute top-[4.5rem] left-0 right-0 bottom-0 w-full 
     uppercase bg-white list-none justify-around items-center transform transition-all duration-300 text-center 
-    lg:top-[6rem] lg:flex-row 
+    lg:top-[6rem] lg:flex-row z-50	lg:bg-transparent
     ${isObjectivesMenuOpen ? "opacity-100 transform translate-y-0" : "opacity-0 pointer-events-none"}`;
 
   const menuListClasses = `top-[4.5rem] h-[calc(100vh-4.5rem)] w-full uppercase bg-white list-none 
     flex flex-col justify-around items-center 
     transform translate-x-[100vw] transition-all duration-300 text-center fixed inset-0
     lg:bg-[var(--secondaryColor)] lg:static lg:max-h-12 lg:flex-row lg:justify-around lg:opacity-100 lg:transform-none 
-    ${isMobileMenuOpen ? "opacity-100 transform -translate-x-[0vw]" : ""}`;
+    ${isMobileMenuOpen ? "opacity-100 transform translate-x-[0vw]" : ""}`;
 
   const objectivesButtonClasses = `font-[var(--secondaryFont)] text-[var(--darkColor)] no-underline bg-transparent 
     hover:bg-transparent hover:text-[var(--darkColor)] 
