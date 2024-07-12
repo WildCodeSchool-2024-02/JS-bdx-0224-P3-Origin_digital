3ef30a796f134716bec6b48297c4e543b3bbf615
Franck <contact@franckwebpro.com>
2024-07-12 12:20:26 +0200

    finished category page

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -11,11 +11,7 @@ const browse = async (req, res, next) => {
 
 const read = async (req, res, next) => {
   try {
-    const category = await tables.category.read(
-      req.params.id,
-      req.params.id,
-      req.params.id
-    );
+    const category = await tables.category.read(req.params.id);
     if (category == null) {
       res.sendStatus(404);
     } else {
