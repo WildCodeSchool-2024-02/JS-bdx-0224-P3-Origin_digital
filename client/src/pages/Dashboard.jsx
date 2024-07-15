6771c5e3ee7110682b041dc1acaa1f33c50d7c57
Franck <contact@franckwebpro.com>
2024-07-15 10:24:31 +0200

    fix

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-import { Column, Table, TableHeader, TableBody } from "react-aria-components";
-import { useState } from "react";
-import DashboardVideo from "../components/DashboardVideo";
-import DashboardModal from "../components/DashboardModal";
-
-export default function Dashboard() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  //   const [toModify, setToModify] = useState(false);
-
-  const handleOpenModal = () => setIsModalOpen(!isModalOpen);
-  //   const handleOpenModalModify = () => [
-  //     setIsModalOpen(!isModalOpen),
-  //     setToModify(!toModify),
-  //   ];
-
-  return (
-    <>
-      <section>
-        <input
-          type="text"
-          placeholder="Rechercher une vidéo.."
-          className="bg-(var"
-        />
-        <button
-          type="button"
-          onClick={handleOpenModal}
-          className="flex items-center"
-        >
-          +
-        </button>
-        <section className="overflow-x-auto rounded-xl">
-          <Table aria-label="Files" selectionMode="multiple">
-            <TableHeader className=" bg-[var(--primaryLight)]">
-              <Column isRowHeader className="w-60">
-                Vos vidéos
-              </Column>
-              <Column className="w-28">Catégorie</Column>
-              <Column className="w-56">Tag</Column>
-              <Column className="w-28">Visibilité</Column>
-              <Column className="w-16">Vue</Column>
-              <Column className="w-36">Publication</Column>
-              <Column className="w-36">Modification</Column>
-            </TableHeader>
-            <TableBody>
-              <DashboardVideo handleOpenModal={handleOpenModal} />
-              <DashboardVideo handleOpenModal={handleOpenModal} />
-              <DashboardVideo handleOpenModal={handleOpenModal} />
-              <DashboardVideo handleOpenModal={handleOpenModal} />
-            </TableBody>
-          </Table>
-        </section>
-      </section>
-      <DashboardModal
-        displayClass={isModalOpen ? "grid" : "none"}
-        isModalOpen={isModalOpen}
-        handleOpenModal={handleOpenModal}
-        toModify
-      />
-    </>
-  );
-}
-=======
 import { Column, Table, TableHeader, TableBody } from "react-aria-components";
 import DashboardVideo from "../components/DashboardVideo";
 
@@ -72,7 +9,12 @@ export default function Dashboard() {
         placeholder="Rechercher une vidéo.."
         className="mt-6 lg:mr-5 bg-primary-color rounded-full p-1 lg:p-2 pl-4 ml-4 md:ml-8 lg:ml-12 w-[90vw] lg:w-auto"
       />
-      <button type="button" className="p-0 mt-6 ml-4 px-4 h-8 lg:h-11  rounded-full overflow-hidden w-[90vw] lg:w-auto">+ Ajouter une vidéo</button>
+      <button
+        type="button"
+        className="p-0 mt-6 ml-4 px-4 h-8 lg:h-11  rounded-full overflow-hidden w-[90vw] lg:w-auto"
+      >
+        + Ajouter une vidéo
+      </button>
       <section className="overflow-x-auto rounded-xl">
         <h2>Votre Tableau de bord</h2>
         <Table className="w-[90vw] mx-auto">
@@ -85,7 +27,9 @@ export default function Dashboard() {
             <Column className="px-3 lg:p-0 lg:w-28">Visibilité</Column>
             <Column className="px-3 lg:p-0 lg:w-16">Vue</Column>
             <Column className="px-3 lg:p-0 lg:w-36">Publication</Column>
-            <Column className="px-3 lg:p-0 lg:w-36 rounded-tr-3xl">Modification</Column>
+            <Column className="px-3 lg:p-0 lg:w-36 rounded-tr-3xl">
+              Modification
+            </Column>
           </TableHeader>
           <TableBody className="[&>*:nth-child(even)]:bg-secondary-color ">
             <DashboardVideo />
@@ -96,4 +40,3 @@ export default function Dashboard() {
     </>
   );
 }
->>>>>>> a544384794d8c3897bed38d10985055818d8c71d
