140bb943b71d487def0a1dd6240e44df39099bed
sachaig <sacha.isambourg01@gmail.com>
2024-07-21 12:25:54 +0200

    better homepage

--- client/src/pages/MyAccount.jsx
+++ client/src/pages/MyAccount.jsx
@@ -4,7 +4,7 @@ import LoggedContext from "../context/LoggedContext";
 
 function MyAccount() {
   
-  const { isCoach } = useContext(LoggedContext);
+  const { userData } = useContext(LoggedContext);
 
   return (
     <>
@@ -12,10 +12,10 @@ function MyAccount() {
       <ul className="flex flex-col justify-start items-start bg-secondary-color rounded-md mx-5 mb-8 p-5 md:mb-60 lg:mb-52">
         <h3>Information du compte</h3>
         <li>Status: abonné</li>
-        <li>Prénom: {isCoach.firstname} </li>
-        <li>Nom: {isCoach.lastname} </li>
-        <li>Adresse mail: {isCoach.email}</li>
-        {(isCoach.role_id === 3) && <Link to="/dashboard" className="rounded-xl bg-primary-color h-8 px-4 py-2 font-semibold transition-all duration-300 ease-linear cursor-pointer flex justify-center items-center hover:text-light-color hover:bg-primary-dark">
+        <li>Prénom: {userData.firstname} </li>
+        <li>Nom: {userData.lastname} </li>
+        <li>Adresse mail: {userData.email}</li>
+        {(userData.role_id === 3) && <Link to="/dashboard" className="rounded-xl bg-primary-color h-8 px-4 py-2 font-semibold transition-all duration-300 ease-linear cursor-pointer flex justify-center items-center hover:text-light-color hover:bg-primary-dark">
           Accéder à mon tableau de bord
         </Link>}
       </ul>
@@ -24,3 +24,4 @@ function MyAccount() {
 }
 
 export default MyAccount;
+
